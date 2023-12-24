5712e9687d58067e55e9dae1b26103cc5a110da1
Jona1056 <3685713940101@ingenieria.usac.edu.gt>
2023-12-24 01:13:42 -0600

    Iniciar Contenedor

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,19 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 import docker
-
+from flask_cors import CORS
 app = Flask(__name__)
-
+#AGREGAR CORS
+CORS(app)
 def convertir_bytes_a_megabytes(bytes_valor):
     return bytes_valor / (1024 * 1024)
 
 def convertir_bytes_a_kilobytes(bytes_valor):
     return bytes_valor / (1024)
 
-@app.route('/container_stats', methods=['GET'])
-def get_container_stats():
+@app.route('/container_selenium', methods=['GET'])
+def get_container_stats_selenium():
+   try:
     print("La solicitud ha sido recibida")
     cliente_docker = docker.from_env()
-    nombre_contenedor = 'stupefied_thompson'  # Reemplaza con el nombre de tu contenedor
+    nombre_contenedor = 'selenium'  # Reemplaza con el nombre de tu contenedor
     contenedor = cliente_docker.containers.get(nombre_contenedor)
     estadisticas = contenedor.stats(stream=True, decode=True)
     primera_actualizacion = next(estadisticas)
@@ -30,26 +32,205 @@ def get_container_stats():
     red_out = primera_actualizacion['networks']['eth0']['rx_dropped']
     red_in = convertir_bytes_a_kilobytes(red_in)
     red_out = convertir_bytes_a_kilobytes(red_out)
+    print(estadisticas)
     return jsonify({
         'ram': {
-            'uso': f"{convertir_bytes_a_megabytes(estadisticas_memoria['usage']):.2f} MB",
-            'limite': f"{convertir_bytes_a_megabytes(estadisticas_memoria['limit']):.2f} MB",
-            'porcentaje': f"{(estadisticas_memoria['usage'] / estadisticas_memoria['limit']) * 100:.2f}%"
+            'uso': f"{convertir_bytes_a_megabytes(estadisticas_memoria['usage']):.2f} ",#MB
+            'limite': f"{convertir_bytes_a_megabytes(estadisticas_memoria['limit']):.2f} ",#MB
+            'porcentaje': f"{(estadisticas_memoria['usage'] / estadisticas_memoria['limit']) * 100:.2f}"#%
         },
         'almacenamiento': {
-            'memory_in': f"{memory_in:.2f} MB",
-            'memory_out': f"{memory_out:.2f} KB",
+            'memory_in': f"{memory_in:.2f} ",#MB
+            'memory_out': f"{memory_out:.2f} ",#KB
             
         },
         'red': {
-            'red_in': f"{red_in:.2f} KB",
-            'red_out': f"{red_out:.2f} KB",
+            'red_in': f"{red_in:.2f} ", #KB
+            'red_out': f"{red_out:.2f} ", #KB
         },
         'cpu': {
-            'porcentaje': f"{porcentaje_cpu:.2f}%"
+            'porcentaje': f"{porcentaje_cpu:.2f}" #%
+        },
+    })
+   except:
+        cliente_docker = docker.from_env()
+        nombre_contenedor = 'selenium'  # Reemplaza con el nombre de tu contenedor
+        contenedor = cliente_docker.containers.get(nombre_contenedor)
+        estadisticas = contenedor.stats(stream=True, decode=True)
+        primera_actualizacion = next(estadisticas)
+        estadisticas_memoria = primera_actualizacion['memory_stats']
+        red_in = primera_actualizacion['networks']['eth0']['rx_bytes']
+        red_out = primera_actualizacion['networks']['eth0']['rx_dropped']
+        red_in = convertir_bytes_a_kilobytes(red_in)
+        red_out = convertir_bytes_a_kilobytes(red_out)
+        estadisticas_almacenamiento = primera_actualizacion['blkio_stats']
+        porcentaje_cpu = primera_actualizacion['cpu_stats']['cpu_usage']['total_usage'] / primera_actualizacion['cpu_stats']['system_cpu_usage'] * 100
+        return jsonify({
+        'ram': {
+            'uso': f"{convertir_bytes_a_megabytes(estadisticas_memoria['usage']):.2f} ",#MB
+            'limite': f"{convertir_bytes_a_megabytes(estadisticas_memoria['limit']):.2f} ",#MB
+            'porcentaje': f"{(estadisticas_memoria['usage'] / estadisticas_memoria['limit']) * 100:.2f}"#%
+        },
+        'almacenamiento': {
+            'memory_in': f"0",#MB
+            'memory_out': f"0",#KB
+        },
+        'red': {
+            'red_in': f"{red_in:.2f} ", #KB
+            'red_out': f"{red_out:.2f} ", #KB
+        },
+        'cpu': {
+            'porcentaje': f"{porcentaje_cpu:.2f}" #%
         },
     })
 
-
+@app.route('/container_playwright', methods=['GET'])
+def get_container_stats_playwright():
+    print("La solicitud ha sido recibida")
+    cliente_docker = docker.from_env()
+    nombre_contenedor = 'playwright'  # Reemplaza con el nombre de tu contenedor
+    contenedor = cliente_docker.containers.get(nombre_contenedor)
+    estadisticas = contenedor.stats(stream=True, decode=True)
+    primera_actualizacion = next(estadisticas)
+    estadisticas_memoria = primera_actualizacion['memory_stats']
+    estadisticas_almacenamiento = primera_actualizacion['blkio_stats']
+    porcentaje_cpu = primera_actualizacion['cpu_stats']['cpu_usage']['total_usage'] / primera_actualizacion['cpu_stats']['system_cpu_usage'] * 100
+    pids = primera_actualizacion['pids_stats']['current']
+    estadisticas_block_io = primera_actualizacion['blkio_stats']['io_service_bytes_recursive']
+    memory_in = primera_actualizacion['blkio_stats']['io_service_bytes_recursive'][0]['value']
+    memory_out = primera_actualizacion['blkio_stats']['io_service_bytes_recursive'][1]['value']
+    memory_in = convertir_bytes_a_megabytes(memory_in)
+    memory_out = convertir_bytes_a_kilobytes(memory_out)
+    red_in = primera_actualizacion['networks']['eth0']['rx_bytes']
+    red_out = primera_actualizacion['networks']['eth0']['rx_dropped']
+    red_in = convertir_bytes_a_kilobytes(red_in)
+    red_out = convertir_bytes_a_kilobytes(red_out)
+    print(estadisticas)
+    return jsonify({
+        'ram': {
+            'uso': f"{convertir_bytes_a_megabytes(estadisticas_memoria['usage']):.2f} ",#MB
+            'limite': f"{convertir_bytes_a_megabytes(estadisticas_memoria['limit']):.2f} ",#MB
+            'porcentaje': f"{(estadisticas_memoria['usage'] / estadisticas_memoria['limit']) * 100:.2f}"#%
+        },
+        'almacenamiento': {
+            'memory_in': f"{memory_in:.2f} ",#MB
+            'memory_out': f"{memory_out:.2f} ",#KB
+            
+        },
+        'red': {
+            'red_in': f"{red_in:.2f} ", #KB
+            'red_out': f"{red_out:.2f} ", #KB
+        },
+        'cpu': {
+            'porcentaje': f"{porcentaje_cpu:.2f}" #%
+        },
+    })
+    
+@app.route('/container_redis', methods=['GET'])
+def get_container_stats_redis():
+    print("La solicitud ha sido recibida")
+    cliente_docker = docker.from_env()
+    nombre_contenedor = 'redis'  # Reemplaza con el nombre de tu contenedor
+    contenedor = cliente_docker.containers.get(nombre_contenedor)
+    estadisticas = contenedor.stats(stream=True, decode=True)
+    primera_actualizacion = next(estadisticas)
+    estadisticas_memoria = primera_actualizacion['memory_stats']
+    estadisticas_almacenamiento = primera_actualizacion['blkio_stats']
+    porcentaje_cpu = primera_actualizacion['cpu_stats']['cpu_usage']['total_usage'] / primera_actualizacion['cpu_stats']['system_cpu_usage'] * 100
+    pids = primera_actualizacion['pids_stats']['current']
+    estadisticas_block_io = primera_actualizacion['blkio_stats']['io_service_bytes_recursive']
+    memory_in = primera_actualizacion['blkio_stats']['io_service_bytes_recursive'][0]['value']
+    memory_out = primera_actualizacion['blkio_stats']['io_service_bytes_recursive'][1]['value']
+    memory_in = convertir_bytes_a_megabytes(memory_in)
+    memory_out = convertir_bytes_a_kilobytes(memory_out)
+    red_in = primera_actualizacion['networks']['eth0']['rx_bytes']
+    red_out = primera_actualizacion['networks']['eth0']['rx_dropped']
+    red_in = convertir_bytes_a_kilobytes(red_in)
+    red_out = convertir_bytes_a_kilobytes(red_out)
+    print(estadisticas)
+    return jsonify({
+        'ram': {
+            'uso': f"{convertir_bytes_a_megabytes(estadisticas_memoria['usage']):.2f} ",#MB
+            'limite': f"{convertir_bytes_a_megabytes(estadisticas_memoria['limit']):.2f} ",#MB
+            'porcentaje': f"{(estadisticas_memoria['usage'] / estadisticas_memoria['limit']) * 100:.2f}"#%
+        },
+        'almacenamiento': {
+            'memory_in': f"{memory_in:.2f} ",#MB
+            'memory_out': f"{memory_out:.2f} ",#KB
+            
+        },
+        'red': {
+            'red_in': f"{red_in:.2f} ", #KB
+            'red_out': f"{red_out:.2f} ", #KB
+        },
+        'cpu': {
+            'porcentaje': f"{porcentaje_cpu:.2f}" #%
+        },
+    })
+    
+@app.route('/container_ngnix', methods=['GET'])
+def get_container_stats_ngnix():
+    print("La solicitud ha sido recibida")
+    cliente_docker = docker.from_env()
+    nombre_contenedor = 'nginx'  # Reemplaza con el nombre de tu contenedor
+    contenedor = cliente_docker.containers.get(nombre_contenedor)
+    estadisticas = contenedor.stats(stream=True, decode=True)
+    primera_actualizacion = next(estadisticas)
+    estadisticas_memoria = primera_actualizacion['memory_stats']
+    estadisticas_almacenamiento = primera_actualizacion['blkio_stats']
+    porcentaje_cpu = primera_actualizacion['cpu_stats']['cpu_usage']['total_usage'] / primera_actualizacion['cpu_stats']['system_cpu_usage'] * 100
+    pids = primera_actualizacion['pids_stats']['current']
+    estadisticas_block_io = primera_actualizacion['blkio_stats']['io_service_bytes_recursive']
+    memory_in = primera_actualizacion['blkio_stats']['io_service_bytes_recursive'][0]['value']
+    memory_out = primera_actualizacion['blkio_stats']['io_service_bytes_recursive'][1]['value']
+    memory_in = convertir_bytes_a_megabytes(memory_in)
+    memory_out = convertir_bytes_a_kilobytes(memory_out)
+    red_in = primera_actualizacion['networks']['eth0']['rx_bytes']
+    red_out = primera_actualizacion['networks']['eth0']['rx_dropped']
+    red_in = convertir_bytes_a_kilobytes(red_in)
+    red_out = convertir_bytes_a_kilobytes(red_out)
+    print(estadisticas)
+    return jsonify({
+        'ram': {
+            'uso': f"{convertir_bytes_a_megabytes(estadisticas_memoria['usage']):.2f} ",#MB
+            'limite': f"{convertir_bytes_a_megabytes(estadisticas_memoria['limit']):.2f} ",#MB
+            'porcentaje': f"{(estadisticas_memoria['usage'] / estadisticas_memoria['limit']) * 100:.2f}"#%
+        },
+        'almacenamiento': {
+            'memory_in': f"{memory_in:.2f} ",#MB
+            'memory_out': f"{memory_out:.2f} ",#KB
+            
+        },
+        'red': {
+            'red_in': f"{red_in:.2f} ", #KB
+            'red_out': f"{red_out:.2f} ", #KB
+        },
+        'cpu': {
+            'porcentaje': f"{porcentaje_cpu:.2f}" #%
+        },
+    })
+    
+@app.route('/container_start', methods=['POST'])
+def iniciar_contenedor():
+    nombre = request.get_json()
+    nombre = nombre['contenedor']
+    cliente_docker = docker.from_env()
+    contenedor = cliente_docker.containers.get(nombre)
+    #verificar si el contenedor esta corriendo
+    print(contenedor.status)
+    if contenedor.status == 'running':
+        print("El contenedor ya esta corriendo")
+        return jsonify({
+            'status': 200,
+            'mensaje': 'El contenedor ya esta corriendo'
+        })
+    else:
+        contenedor.start()
+        return jsonify({
+            'status': 400,
+            'mensaje': 'Contenedor iniciado'
+        })
+ 
+    
 if __name__ == '__main__':
     app.run(debug=True,port=8010)
--- dashboard_web/src/App.jsx
+++ dashboard_web/src/App.jsx
@@ -1,35 +1,283 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React, { useState, useEffect } from 'react';
 
+import './App.css';
+const App = () => {
+  const [stats_selenium, setStats] = useState(null);
+  const [stats_playwright, setStats2] = useState(null);
+  const [stats_redis, setStats3] = useState(null);
+  const [stats_load_balancer, setStats4] = useState(null);
+
+
+  const obtenerEstadisticas_selenium = async () => {
+    try {
+      const respuesta = await fetch('http://localhost:8010/container_selenium');
+      console.log(respuesta)
+      const datos = await respuesta.json();
+      setStats(datos);
+
+    } catch (error) {
+      console.error('Error al obtener estadísticas:', error);
+    }
+  };
+  const obtenerEstadisticas_playwright = async () => {
+    try {
+      const respuesta = await fetch('http://localhost:8010/container_playwright');
+      console.log(respuesta)
+      const datos = await respuesta.json();
+      setStats2(datos);
+
+    } catch (error) {
+      console.error('Error al obtener estadísticas:', error);
+    }
+  }
+  const obtenerEstadisticas_redis = async () => {
+    try {
+      const respuesta = await fetch('http://localhost:8010/container_redis');
+      console.log(respuesta)
+      const datos = await respuesta.json();
+      setStats3(datos);
+
+    } catch (error) {
+      console.error('Error al obtener estadísticas:', error);
+    }
+  }
+
+  const obtenerEstadisticas_load_balancer = async () => {
+    try {
+      const respuesta = await fetch('http://localhost:8010/container_ngnix');
+      console.log(respuesta)
+      const datos = await respuesta.json();
+      setStats4(datos);
+
+    } catch (error) {
+      console.error('Error al obtener estadísticas:', error);
+    }
+  }
+
+  const IniciarContenedor = async (contenedor) => {
+    try {
+      const respuesta = await fetch('http://localhost:8010/container_start', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ contenedor: contenedor })
+      });
+      const datos = await respuesta.json();
+      if (datos.status === 400){
+        alert("El contenedor se inicio correctamente")
+      }else{
+        alert("El contenedor ya esta iniciado")
+      }
+     
+      
+    } catch (error) {
+      alert("Error al iniciar el contenedor")
+    }
+  }
+
+
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      obtenerEstadisticas_selenium();
+      obtenerEstadisticas_playwright();
+      obtenerEstadisticas_redis();
+      obtenerEstadisticas_load_balancer();
+      console.log("Obteniendo estadisticas")
+    }, 10000);
+    return () => clearInterval(intervalo);
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="container">
+        <div className="content">
+          <h1>DashBoard Web</h1>
+          <h2>Estadisticas del Contenedor: Selenium</h2>
+          {stats_selenium ? (
+            <div className="dash">
 
-export default App
+              <div className="card">
+                <p className="heading">Almacenamiento</p>
+                <p>Memory Out: {stats_selenium.almacenamiento.memory_out} MB <br /> Memory In: {stats_selenium.almacenamiento.memory_in} KB</p>
+              </div>
+              <div className="card">
+                <p className="heading">CPU</p>
+                <p>Porcentaje: {stats_selenium.cpu.porcentaje}%</p>
+              </div>
+              <div className="card">
+                <p className="heading">RAM</p>
+                <p>limite: {stats_selenium.ram.limite} MB
+                  <br /> Porcentaje: {stats_selenium.ram.porcentaje}%
+                  <br /> Uso: {stats_selenium.ram.uso} MB
+                </p>
+              </div>
+              <div className="card">
+                <p className="heading">RED</p>
+                <p>Red IN: {stats_selenium.red.red_in} KB
+                  <br /> Red OUT: {stats_selenium.red.red_out}KB
+
+                </p>
+              </div>
+            </div>
+
+          ) : (
+            <div className="loading-wave">
+              <div className="loading-bar"></div>
+              <div className="loading-bar"></div>
+              <div className="loading-bar"></div>
+              <div className="loading-bar"></div>
+            </div>
+          )}
+          <div className="card2">
+          <button className="btn" onClick={() => IniciarContenedor("selenium")}> Inicar Contenedor 
+          </button>
+          <button className="btn" onClick={() => ReiniciarContenedor("selenium")}> Reiniciar Contenedor 
+          </button>
+          <button className="btn" onClick={() => ApagarContenedor("selenium")}> Apagar Contenedor 
+          </button>
+          </div>
+          <h2>Estadisticas del Contenedor: Playwright</h2>
+          {stats_playwright ? (
+            <div className="dash">
+
+              <div className="card">
+                <p className="heading">Almacenamiento</p>
+                <p>Memory Out: {stats_playwright.almacenamiento.memory_out} MB <br /> Memory In: {stats_playwright.almacenamiento.memory_in} KB</p>
+              </div>
+              <div className="card">
+                <p className="heading">CPU</p>
+                <p>Porcentaje: {stats_playwright.cpu.porcentaje}%</p>
+              </div>
+              <div className="card">
+                <p className="heading">RAM</p>
+                <p>limite: {stats_playwright.ram.limite} MB
+                  <br /> Porcentaje: {stats_playwright.ram.porcentaje}%
+                  <br /> Uso: {stats_playwright.ram.uso} MB
+                </p>
+              </div>
+              <div className="card">
+                <p className="heading">RED</p>
+                <p>Red IN: {stats_playwright.red.red_in} KB
+                  <br /> Red OUT: {stats_playwright.red.red_out}KB
+
+                </p>
+              </div>
+            </div>
+
+          ) : (
+            <div className="loading-wave">
+              <div className="loading-bar"></div>
+              <div className="loading-bar"></div>
+              <div className="loading-bar"></div>
+              <div className="loading-bar"></div>
+            </div>
+          )}
+          <div className="card2">
+          <button className="btn"> Inicar Contenedor
+          </button>
+          <button className="btn"> Reiniciar Contenedor
+          </button>
+          <button className="btn"> Apagar Contenedor
+          </button>
+          </div>
+
+          <h2>Estadisticas del Contenedor: Redis</h2>
+          {stats_redis ? (
+            <div className="dash">
+
+              <div className="card">
+                <p className="heading">Almacenamiento</p>
+                <p>Memory Out: {stats_redis.almacenamiento.memory_out} MB <br /> Memory In: {stats_redis.almacenamiento.memory_in} KB</p>
+              </div>
+              <div className="card">
+                <p className="heading">CPU</p>
+                <p>Porcentaje: {stats_redis.cpu.porcentaje}%</p>
+              </div>
+              <div className="card">
+                <p className="heading">RAM</p>
+                <p>limite: {stats_redis.ram.limite} MB
+                  <br /> Porcentaje: {stats_redis.ram.porcentaje}%
+                  <br /> Uso: {stats_redis.ram.uso} MB
+                </p>
+              </div>
+              <div className="card">
+                <p className="heading">RED</p>
+                <p>Red IN: {stats_redis.red.red_in} KB
+                  <br /> Red OUT: {stats_redis.red.red_out}KB
+
+                </p>
+              </div>
+            </div>
+
+          ) : (
+            <div className="loading-wave">
+              <div className="loading-bar"></div>
+              <div className="loading-bar"></div>
+              <div className="loading-bar"></div>
+              <div className="loading-bar"></div>
+            </div>
+          )}
+          <div className="card2">
+          <button className="btn"> Inicar Contenedor
+          </button>
+          <button className="btn"> Reiniciar Contenedor
+          </button>
+          <button className="btn"> Apagar Contenedor
+          </button>
+          </div>
+
+          <h2>Estadisticas del Contenedor: Load Balancer</h2>
+          {stats_load_balancer ? (
+            <div className="dash">
+
+              <div className="card">
+                <p className="heading">Almacenamiento</p>
+                <p>Memory Out: {stats_load_balancer.almacenamiento.memory_out} MB <br /> Memory In: {stats_load_balancer.almacenamiento.memory_in} KB</p>
+              </div>
+              <div className="card">
+                <p className="heading">CPU</p>
+                <p>Porcentaje: {stats_load_balancer.cpu.porcentaje}%</p>
+              </div>
+              <div className="card">
+                <p className="heading">RAM</p>
+                <p>limite: {stats_load_balancer.ram.limite} MB
+                  <br /> Porcentaje: {stats_load_balancer.ram.porcentaje}%
+                  <br /> Uso: {stats_load_balancer.ram.uso} MB
+                </p>
+              </div>
+              <div className="card">
+                <p className="heading">RED</p>
+                <p>Red IN: {stats_load_balancer.red.red_in} KB
+                  <br /> Red OUT: {stats_load_balancer.red.red_out}KB
+
+                </p>
+              </div>
+            </div>
+
+          ) : (
+            <div className="loading-wave">
+              <div className="loading-bar"></div>
+              <div className="loading-bar"></div>
+              <div className="loading-bar"></div>
+              <div className="loading-bar"></div>
+            </div>
+          )}
+          <div className="card2">
+          <button className="btn"> Inicar Contenedor
+          </button>
+          <button className="btn"> Reiniciar Contenedor
+          </button>
+          <button className="btn"> Apagar Contenedor
+          </button>
+          </div>
+          <div><br/></div>
+
+        </div>
+      </div>
+    </>
+
+  );
+};
+
+export default App;
